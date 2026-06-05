0bdf5c5b1d4122f7e5cd842f63e103d41bcb2c4d
shiba <shiba.spj@hotmail.com>
2026-06-06 01:57:45 +0530

    Initialize WatchVideoViewModel for the watch video screen
    This change introduces the `WatchVideoViewModel` class to support the watch video user interface, providing a foundation for managing UI-related data in a lifecycle-conscious way.
    
    Key adjustments include:
    
    *   **ViewModel Implementation:**
        *   Created `WatchVideoViewModel.java` as a subclass of `androidx.lifecycle.ViewModel`.
        *   Added a `LoggerUtils` instance to facilitate logging within the ViewModel.

---
+++ app/src/main/java/userInterface/watchVideo/WatchVideoViewModel.java
@@ -0,0 +1,10 @@
+package userInterface.watchVideo;
+
+import androidx.lifecycle.ViewModel;
+
+import coreUtils.library.process.LoggerUtils;
+
+public class WatchVideoViewModel extends ViewModel {
+	private final LoggerUtils logger = LoggerUtils.from(getClass());
+	
+}
